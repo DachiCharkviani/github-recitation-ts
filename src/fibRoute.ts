c84e766e3fe4a4515d239eb3911709ce392502b2
Dachi <user@MacBook-Air-10.local>
2024-09-01 09:38:01 -0700

    Fixed issues

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,7 +2,7 @@
 
 const fibonacci = require("./fib");
 
-export default (req, res) => {
+export default (req: { params: {num: string}}, res: { send: (body: string) => void}): void => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
